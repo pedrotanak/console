2f7608e64f32ec84906ea4a610f575b7daaf7760
pedrotanak <106190056+pedrotanak@users.noreply.github.com>
2022-12-01 16:56:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Meu projeto de Console");
+var guardaPrimeironome="Pedro";
+var guardaSobrenome=" Tanaka";
+var NomeCompleto=guardaPrimeironome+guardaSobrenome;
+var A=10;
+var B=5;
+var Soma=A+B;
+console.log("A+B="+Soma)
+console.log(NomeCompleto);
